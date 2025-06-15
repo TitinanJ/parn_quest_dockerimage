ec164e0150f750dbd8b38d40fdb3d8d8d64b2314
“parn” <titinanjaem@gmail.com>
2025-06-15 23:38:40 +0700

    migrate model quest status default: false

--- db/migrate/20250615162636_create_quests.rb
+++ db/migrate/20250615162636_create_quests.rb
@@ -2,7 +2,7 @@ class CreateQuests < ActiveRecord::Migration[8.0]
   def change
     create_table :quests do |t|
       t.string :name
-      t.boolean :status
+      t.boolean :status, default: false
 
       t.timestamps
     end
