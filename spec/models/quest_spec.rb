80542085426fd885ae27ba6fc6b9d4134fdbebf5
“parn” <titinanjaem@gmail.com>
2025-06-16 01:05:10 +0700

    create quests unit test

--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Quest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Quest success" do
+    it "is valid with valid attributes" do
+      quest = Quest.new(name: "Test Quest", status: false)
+      expect(quest).to be_valid
+      expect(quest.name).to eq("Test Quest")
+      expect(quest.status).to eq(false)
+    end
+  end
+
+  describe "Quest validations" do
+    it "is valid with a name" do
+      quest = Quest.new(name: "Test Quest", status: false)
+      expect(quest).to be_valid
+      expect(quest.name).to eq("Test Quest")
+      expect(quest.status).to eq(false)
+    end
+
+    it "is not valid without a name" do
+      quest = Quest.new(status: false)
+      expect(quest).not_to be_valid
+      expect(quest.errors[:name]).to include("can't be blank")
+    end
+
+    it "is valid without a status" do
+      quest = Quest.new(name: "Test Quest")
+      expect(quest).to be_valid
+      expect(quest.status).to be_nil
+    end
+  end
 end
