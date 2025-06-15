1ec770bd2b749360d64d5a96bba82432209291af
“parn” <titinanjaem@gmail.com>
2025-06-16 00:07:23 +0700

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get "brag/index"
   root "quests#index"
-  resources :quests
+  resources :quests do
+    member do
+      patch :toggle_complete
+    end
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
