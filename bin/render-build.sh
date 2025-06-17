234839aa14f1454578495453df0ade9cdec25766
“parn” <titinanjaem@gmail.com>
2025-06-17 23:59:48 +0700

    file for deploy

---
+++ bin/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+
+bundle install
+
+yarn install --check-files
+
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+
+bundle exec rake db:migrate
